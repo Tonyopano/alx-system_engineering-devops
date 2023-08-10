27bf8924247a8749aa52495c79c0b04474a43908
Tonyopano <antonyopano@gmail.com>
2023-08-10 12:46:45 -0700

    Puppet debugging code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
-# Fixes bad `phpp` extensions to `php`
+# A puppet manuscript to replace a line in a file on a server
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
